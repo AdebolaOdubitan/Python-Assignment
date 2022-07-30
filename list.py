9df481a5ae9f6bdf299a2f79c65751b428997c0b
Adebola Odubitan <debolaodubitan@gmail.com>
2022-07-30 07:41:47 +0100

    Adding Example.py

--- list.py
+++ list.py
@@ -4,5 +4,4 @@ from tokenize import Number
 
 Number = [10,20,33,46,55,62,70]
 result = list(filter (lambda X:(X%5==0),Number))
-print ('Numbers that are divisible by 5 are:', result)
-
+print ('Numbers that are divisible by 5 are):
